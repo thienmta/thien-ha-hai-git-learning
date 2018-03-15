ff66689892173ef89482336ecdb41edb2f4f4670
MinhHai <haipm@deha-soft.com>
2018-03-15 11:45:50 +0700

    edit test2.php

--- test2.php
+++ test2.php
@@ -1,22 +1,10 @@
 <?php
-	
 	class math{
-		
-			include "/add.php";
-		include "/sub.php";
 		function multi($a,$b){
 			$multi =  $a * $b;
-			return $multi;
+			echo $multi;
 		}
-	
 	}
-	$a=2;
-	$b=5;
-	$multil=math.multi($a,$b);
-	echo $multil;
-$add=math.add($a,$b);
-	echo $add;
-$sub=math.Sub($a,$b);
-	echo $sub;
-	
+	$math = new math();
+	$math -> multi(1,2);
 ?>
