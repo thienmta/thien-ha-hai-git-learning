a1c4043c6232d04d5ae49faedeb3a739bb7598e3
hapro955 <hapro955@gamil.com>
2018-03-15 11:48:04 +0700

    fix test2

--- test2.php
+++ test2.php
@@ -1,9 +1,9 @@
 <?php
-	
+		include "add.php";
+		include "sub.php";
 	class math{
 		
-			include "/add.php";
-		include "/sub.php";
+		
 		function multi($a,$b){
 			$multi =  $a * $b;
 			return $multi;
@@ -12,11 +12,12 @@
 	}
 	$a=2;
 	$b=5;
-	$multil=math.multi($a,$b);
+	$math= new math;
+	$multil=$math->multi($a,$b);
 	echo $multil;
-$add=math.add($a,$b);
-	echo $add;
-$sub=math.Sub($a,$b);
-	echo $sub;
+	
+	echo $add($a,$b);
+
+	echo $sub($a,$b);
 	
 ?>
