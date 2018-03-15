a8dc5e23c029cb3148aebdddb4f5cbeffb21b31b
MinhHai <haipm@deha-soft.com>
2018-03-15 14:01:37 +0700

    init class

---
+++ Math.php
@@ -0,0 +1,23 @@
+<?php
+
+class Math {
+    public function add()
+    {
+
+    }
+
+    public function sub()
+    {
+
+    }
+
+    public function multi()
+    {
+
+    }
+
+    public function divide()
+    {
+
+    }
+}
